144a8482319f5d16d72944556782b98b4e198ad6
Space_Cowboy <sfferra@amazon.com>
2017-11-10 21:55:50 -0700

    forhomestudy

---
+++ Django/users/apps/user_login/views.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HtppResponse("Stop wasting time here and work on shell queries!")
